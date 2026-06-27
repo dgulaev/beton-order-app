40b383260ff5ec71256d6396707825bdb8b1d8fe
Dmitry Gulaev <gudman@Mac-mini-Dmitry.local>
2026-06-27 23:36:21 +0300

    update:  переработал страницу CRM системы для клиентов и стафа. Добавил назначение кураторов и статистику по клиентам 27.06.26 23 57

--- supabase-local/task_comments.sql
+++
@@ -1,9 +0,0 @@
-create table public.task_comments (
-  id bigserial not null,
-  task_id bigint null,
-  user_id bigint not null,
-  comment text not null,
-  created_at timestamp with time zone null default now(),
-  constraint task_comments_pkey primary key (id),
-  constraint task_comments_task_id_fkey foreign KEY (task_id) references tasks (id)
-) TABLESPACE pg_default;
